3eb020b372397cb38dce976d18ae063fc614d68b
Daniel Perkins II <daniel.perkins99@gmail.com>
2020-11-19 20:32:16 -0600

    config for deploy

--- lina-liu-art/components/Layout.js
+++ lina-liu-art/components/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Head from 'next/head'
 
-export default function Layout() {
+export default function Layout({ children, title }) {
     return (
         <div>
             <Head>
+                <title>{ title ? `Lina Liu Art :: ${title}` : 'Lina Liu Art'}</title>
                 <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
             </Head>
-            <div>This is the layout</div>
+            <div>{ children }</div>
         </div>
     )
 }
