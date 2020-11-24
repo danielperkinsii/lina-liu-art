84153efde42bdf3964e4ee4ec9fec3feda62693e
Daniel Perkins II <daniel.perkins99@gmail.com>
2020-11-24 15:09:50 -0600

    purgecss config

--- lina-liu-art/components/post.js
+++ lina-liu-art/components/post.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Post({ alt, date, title, url }) {
     return (
       <div className="container">
         <a href={null}>
-          <img alt={alt} src={`https:${url}`} />
+          <img 
+          alt={alt} 
+          src={`https:${url}`} 
+          
+          />
         </a>
         <div className="text">
           <h2 className="text-blue-500">{title}</h2>
