4ea372470306c2297e52bfd202b38c1fcc6fcf56
Daniel Perkins II <daniel.perkins99@gmail.com>
2020-12-02 09:09:26 -0600

    imported header and global css to index.js

--- lina-liu-art/components/layout.js
+++ lina-liu-art/components/layout.js
@@ -1,27 +1,25 @@
-// this code is from ./components/layout
-
 import React from 'react'
-// import Head from 'next/head'
-// import Header from './header.js'
+import Head from 'next/head'
+import Header from './header.js'
 
 export default function Layout({ children, title }) {
     return (
         <div>
-            {/* <Head>
+            <Head>
                 <title>{ title ? `Lina Liu Art :: ${title}` : 'Lina Liu Art'}</title>
                 
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            </Head> */}
-            {/* <Header /> */}
+            </Head>
+            <Header />
             <div className='mx-4 px-4'>{ children }</div>
-            {/* <style jsx global>{`
+            <style jsx global>{`
                 html,
                 body {
                     font-family: 'Cormorant', serif;
                     overflow-x: hidden;
                 }
-            `}</style> */}
+            `}</style>
         </div>
     )
 }
