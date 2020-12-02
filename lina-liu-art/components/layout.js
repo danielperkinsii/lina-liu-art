e5f149d4b7b5ef14d4d922d11d887b4e938ac7f3
Daniel Perkins II <daniel.perkins99@gmail.com>
2020-12-02 09:33:22 -0600

    installed latest autoprefixer

--- lina-liu-art/components/layout.js
+++ lina-liu-art/components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import Header from './header.js'
+import Header from './header'
 
 export default function Layout({ children, title }) {
     return (
