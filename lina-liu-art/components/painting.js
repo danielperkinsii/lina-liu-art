03f99b1da3b859dae810797879212536929360f2
Daniel Perkins II <daniel.perkins99@gmail.com>
2020-12-13 11:08:43 -0600

    feat(refactored code): refactored file structures, commented descriptions on functions, renamed post to painting

--- lina-liu-art/components/painting.js
+++ lina-liu-art/components/painting.js
@@ -1,34 +1,37 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 
-function Post({ alt, medium, size, title, url, id }) {
+export default function Painting({ alt, medium, size, title, url, id }) {
+  
   const [showElements, setShowElements] = useState(false)
-
+  
   return (
     <div 
     className='pl-1 pt-1'
     onMouseEnter={() => setShowElements(true)}
-    onMouseLeave={() => setShowElements(false)}
-    >
+    onMouseLeave={() => setShowElements(false)}>
       <Link href={`category/${id}`}>
-          <a className=''>
-              <img className='w-72 h-full'
-              alt={alt} 
-              src={`https://${url}`} 
-              />
-          </a>
+        <a>
+          <img 
+          className='w-72 h-full'
+          alt={alt} 
+          src={`https://${url}`} 
+          />
+        </a>
       </Link>
-      {showElements ? 
-      <div className='sm:flex flex-col hidden sm:w-60 h-28 -mt-32 mb-4 bg-gray-700 opacity-75 hover:opacity-100 transition duration-600 ease-in-out rounded-md shadow-lg mx-auto relative py-2 px-3 shadow-lg text-white'>
+
+      {showElements ? // displays info on mouseEnter
+
+      <div className='sm:flex flex-col hidden sm:w-60 h-28 -mt-32 mb-4 bg-gray-700 opacity-75 hover:opacity-100 rounded-md shadow-lg mx-auto relative py-2 px-3 shadow-lg text-white'>
         <h2 className='text-xl truncate'>{title}</h2>
         <h2 className='truncate'>{medium}{size ? `, ${size}` : null}</h2>
         <Link href={`category/${id}`}>
           <a className='hidden sm:contents text-right p-2 text-blue-400 text-2xl font-bold'>Click to enlarge</a>
         </Link>
       </div>
+
       : null}
+
     </div>
   )
 }
-  
-  export default Post
