5defb09bf0f04ec7e1ba72cea3a745e435e0a23d
Daniel Perkins II <daniel.perkins99@gmail.com>
2020-12-15 10:23:35 -0600

    css change

--- lina-liu-art/components/painting.js
+++ lina-liu-art/components/painting.js
@@ -22,7 +22,7 @@ export default function Painting({ alt, medium, size, title, url, id, query }) {
       {showElements ? // displays info on mouseEnter
 
       <div className='sm:flex flex-col hidden sm:w-60 h-28 -mt-32 mb-4 bg-gray-700 opacity-75 hover:opacity-100 rounded-md shadow-lg mx-auto relative py-2 px-3 shadow-lg text-white'>
-        <h2 className='text-xl truncate bg-yellow-300'>{title}</h2>
+        <h2 className='text-xl truncate'>{title}</h2>
         <h2 className='truncate'>{medium}{size ? `, ${size}` : null}</h2>
         <Link href={`category/${id}`}>
           <a className='hidden sm:contents text-right p-2 text-blue-400 text-2xl font-bold'>Click to enlarge</a>
